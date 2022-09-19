053a43ecaddc6710b7143a8b3bd5dc3a88b92b6e
Goldorak <echelon@free.fr>
2022-09-19 22:44:31 +0200

    * Define a new module "nucleo_message_ids.py" containing nucleo message ids definitions to avoid using numerical values for messages ids in "nucleo_topics.py" (TODO : develop a tool to automatically generate this from the "message_types.hpp" in the nucleo firmware project) * Add message definitions for FPGA ADC read

--- nucleo_topics.py
+++ nucleo_topics.py
@@ -2,6 +2,7 @@ from goldo_main.nucleo.topics._registry import *
 import pb2 as _pb2
 import struct
 import math
+import nucleo_message_ids
 
 _sym_db = _pb2._sym_db
 
@@ -19,21 +20,21 @@ _unpack_heartbeat = struct.Struct('<I').unpack
 
 
 
-@nucleo_out('os/heartbeat', 2)
+@nucleo_out('os/heartbeat', nucleo_message_ids.Heartbeat)
 def heartbeat(payload):
     msg = _sym_db.GetSymbol('goldo.nucleo.Heartbeat')()
     msg.timestamp = _unpack_heartbeat(payload)[0]
     return msg
 
-@nucleo_in('os/ping', 1)
+@nucleo_in('os/ping', nucleo_message_ids.CommUartPing)
 def ping(msg):
     return b''
 
-@nucleo_out('os/heap_stats', 3)
+@nucleo_out('os/heap_stats', nucleo_message_ids.HeapStats)
 def watchdog_state(payload):
     return _pb2.deserialize('goldo.nucleo.FreeRTOSHeapStats', payload[:8])
     
-@nucleo_out('os/tasks_stats', 6)
+@nucleo_out('os/tasks_stats', nucleo_message_ids.TaskStats)
 def os_tasks_stats(payload):
     items = []
     try:
@@ -48,7 +49,7 @@ def os_tasks_stats(payload):
         pass
     return _sym_db.GetSymbol('goldo.nucleo.FreeRTOSTasksStats')(tasks=items)
     
-@nucleo_out('os/dbg_trace', 7)
+@nucleo_out('os/dbg_trace', nucleo_message_ids.DbgTrace)
 def os_dbg_trace(payload):
     return _sym_db.GetSymbol('google.protobuf.BytesValue')(value=payload)
     items = []
@@ -57,67 +58,67 @@ def os_dbg_trace(payload):
         print(a,b,c,d)
     return _sym_db.GetSymbol('goldo.nucleo.FreeRTOSTasksStats')(tasks=items)
 
-@nucleo_out('os/reset', 4)
+@nucleo_out('os/reset', nucleo_message_ids.Reset)
 def os_reset(payload):
     return _sym_db.GetSymbol('google.protobuf.Empty')()
 
-@nucleo_out('os/task_statistics/uart_comm', 300)
+@nucleo_out('os/task_statistics/uart_comm', nucleo_message_ids.UartCommTaskStatistics)
 def os_task_statistics_uart_comm(payload):
     return _pb2.deserialize('goldo.nucleo.statistics.UARTCommTaskStatistics', payload)
 
 
-@nucleo_out('os/task_statistics/odrive_comm', 301)
+@nucleo_out('os/task_statistics/odrive_comm', nucleo_message_ids.ODriveCommTaskStatistics)
 def os_task_statistics_odrive_comm(payload):
     return _pb2.deserialize('goldo.nucleo.statistics.ODriveCommTaskStatistics', payload)
 
-@nucleo_out('os/task_statistics/propulsion', 302)
+@nucleo_out('os/task_statistics/propulsion', nucleo_message_ids.PropulsionTaskStatistics)
 def os_task_statistics_uart_comm(payload):
     return _pb2.deserialize('goldo.nucleo.statistics.PropulsionTaskStatistics', payload)
 
 
-@nucleo_out('watchdog/state', 251)
+@nucleo_out('watchdog/state', nucleo_message_ids.WatchdogStatus)
 def watchdog_state(payload):
     return _pb2.deserialize('goldo.nucleo.WatchdogState', payload[:6])
 
-@nucleo_out('sensors/state', 33)
+@nucleo_out('sensors/state', nucleo_message_ids.SensorsState)
 def sensors_state(payload):
     return _pb2.deserialize('goldo.nucleo.SensorsState', payload)
 
-@nucleo_out('match/timer', 10)
+@nucleo_out('match/timer', nucleo_message_ids.MatchTimer)
 def match_timer(payload):
     return _pb2.deserialize('google.protobuf.Int32Value', payload)
 
-@nucleo_in('match/timer/start', 11)
+@nucleo_in('match/timer/start', nucleo_message_ids.MatchTimerStart)
 def match_timer_start(msg):
     return b''
     
-@nucleo_in('match/timer/stop', 13)
+@nucleo_in('match/timer/stop', nucleo_message_ids.MatchTimerStop)
 def match_timer_start(msg):
     return b''
 
-@nucleo_out('match/end', 12)
+@nucleo_out('match/end', nucleo_message_ids.MatchEnd)
 def match_end(payload):
     return None
 
-@nucleo_in('robot/config/load_begin', 200)
+@nucleo_in('robot/config/load_begin', nucleo_message_ids.RobotConfigLoadBegin)
 def robot_config_load_begin(msg):
     return struct.pack('<H', msg.size)
 
-@nucleo_in('robot/config/load_chunk', 201)
+@nucleo_in('robot/config/load_chunk', nucleo_message_ids.RobotConfigLoadChunk)
 def robot_config_load_chunk(msg):
     return msg.data
 
-@nucleo_in('robot/config/load_end', 202)
+@nucleo_in('robot/config/load_end', nucleo_message_ids.RobotConfigLoadEnd)
 def robot_config_load_end(msg):
     return struct.pack('<H', msg.crc)
 
-@nucleo_out('robot/config/load_status', 203)
+@nucleo_out('robot/config/load_status', nucleo_message_ids.RobotConfigLoadStatus)
 def robot_config_load_status(payload):
     msg = _sym_db.GetSymbol('goldo.nucleo.robot.ConfigLoadStatus')()
     msg.status = struct.unpack('<B', payload)[0]
     return msg
 
-@nucleo_in('odrive/request', 50)
+@nucleo_in('odrive/request', nucleo_message_ids.ODriveRequestPacket)
 def odrive_request(msg):
     # bit 15 is reserved by odrive protocol, bit 14 is set to 0
     buff = struct.pack('<HHH', msg.sequence_number & 0x3fff, msg.endpoint_id, msg.expected_response_size)
@@ -125,18 +126,18 @@ def odrive_request(msg):
     buff += struct.pack('<H', msg.protocol_version)
     return buff
 
-@nucleo_out('odrive/response', 51)
+@nucleo_out('odrive/response', nucleo_message_ids.ODriveResponsePacket)
 def odrive_response(payload):
     msg = _sym_db.GetSymbol('goldo.nucleo.odrive.ResponsePacket')()
     msg.sequence_number = struct.unpack('<H', payload[0:2])[0] & 0x3fff
     msg.payload = payload[2:]
     return msg
 
-@nucleo_out('odrive/telemetry', 52)
+@nucleo_out('odrive/telemetry', nucleo_message_ids.ODriveTelemetry)
 def odrive_telemetry(payload):
     return _pb2.deserialize('goldo.nucleo.odrive.Telemetry', payload)
 
-@nucleo_in('dynamixels/request', 60)
+@nucleo_in('dynamixels/request', nucleo_message_ids.DynamixelsRequest)
 def dynamixels_request(msg):
     buff = struct.pack('<Hbbb',
         msg.sequence_number,
@@ -147,7 +148,7 @@ def dynamixels_request(msg):
     buff = buff + msg.payload
     return buff
 
-@nucleo_out('dynamixels/response', 61)
+@nucleo_out('dynamixels/response', nucleo_message_ids.DynamixelsResponse)
 def dynamixels_response(payload):
     msg = _sym_db.GetSymbol('goldo.nucleo.dynamixels.ResponsePacket')()
     vals = struct.unpack('<HBBB', payload[0:5])
@@ -159,141 +160,149 @@ def dynamixels_response(payload):
     return msg
 
 
-@nucleo_in('fpga/reg/read', 30)
+@nucleo_in('fpga/reg/read', nucleo_message_ids.FpgaReadReg)
 def fpga_reg_read(msg):
     return _pb2.serialize(msg)
 
-@nucleo_out('fpga/reg', 31)
+@nucleo_out('fpga/reg', nucleo_message_ids.FpgaReadRegStatus)
 def fpga_reg_read_status(payload):
     return _pb2.deserialize('goldo.nucleo.fpga.RegReadStatus', payload)
 
-@nucleo_in('fpga/reg/write', 32)
+@nucleo_in('fpga/reg/write', nucleo_message_ids.FpgaWriteReg)
 def fpga_reg_write(msg):
     return _pb2.serialize(msg)
 
+@nucleo_in('fpga/adc/read', nucleo_message_ids.FpgaReadAdc)
+def fpga_adc_read(msg):
+    return _pb2.serialize(msg)
+
+@nucleo_out('fpga/adc/read_out', nucleo_message_ids.FpgaReadAdcOut)
+def fpga_adc_read_out(payload):
+    return _pb2.deserialize('goldo.nucleo.fpga.AdcReadOut', payload)
 
 
-@nucleo_in('odometry/config/get', 210)
+@nucleo_in('odometry/config/get', nucleo_message_ids.OdometryConfigGet)
 def odometry_config_get(msg):
     return b''
 
-@nucleo_out('odometry/config', 211)
+@nucleo_out('odometry/config', nucleo_message_ids.OdometryConfigGetStatus)
 def odometry_config_get_status(payload):
     return _pb2.deserialize('goldo.nucleo.propulsion.OdometryConfig', payload)
 
-@nucleo_in('odometry/config/set', 212)
+@nucleo_in('odometry/config/set', nucleo_message_ids.OdometryConfigSet)
 def odometry_config_set(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/config/get', 215)
+
+@nucleo_in('propulsion/config/get', nucleo_message_ids.PropulsionConfigGet)
 def propulsion_config_get(msg):
     return b''
 
-@nucleo_out('propulsion/config', 216)
+@nucleo_out('propulsion/config', nucleo_message_ids.PropulsionConfigGetStatus)
 def propulsion_config_get_status(payload):
     return _pb2.deserialize('goldo.nucleo.propulsion.PropulsionControllerConfig', payload)
 
-@nucleo_in('propulsion/config/set', 217)
+@nucleo_in('propulsion/config/set', nucleo_message_ids.PropulsionConfigSet)
 def propulsion_config_set(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/enable/set', 100)
+@nucleo_in('propulsion/enable/set', nucleo_message_ids.PropulsionEnableSet)
 def propulsion_enable_set(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/simulation/enable', 110)
+@nucleo_in('propulsion/simulation/enable', nucleo_message_ids.PropulsionSetSimulationMode)
 def propulsion_simulation_enable_set(msg):
     return struct.pack('<B', msg.value)
 
-@nucleo_in('propulsion/motors/enable/set', 101)
+@nucleo_in('propulsion/motors/enable/set', nucleo_message_ids.PropulsionMotorsEnableSet)
 def propulsion_motors_enable_set(msg):
     return struct.pack('<B', msg.value)
 
-@nucleo_in('propulsion/emergency_stop', 107)
+@nucleo_in('propulsion/emergency_stop', nucleo_message_ids.PropulsionEmergencyStop)
 def propulsion_emergency_stop(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/clear_error', 108)
+@nucleo_in('propulsion/clear_error', nucleo_message_ids.PropulsionClearError)
 def propulsion_clear_error(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/motors/velocity_setpoints/set', 102)
+@nucleo_in('propulsion/motors/velocity_setpoints/set', nucleo_message_ids.PropulsionMotorsVelocitySetpointsSet)
 def propulsion_motors_velocity_setpoints_set(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/target_speed/set', 103)
+@nucleo_in('propulsion/target_speed/set', nucleo_message_ids.PropulsionSetTargetSpeed)
 def propulsion_target_speed_set(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/pose/set', 105)
+@nucleo_in('propulsion/pose/set', nucleo_message_ids.PropulsionSetPose)
 def propulsion_pose_set(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/calibrate_odrive', 151)
+@nucleo_in('propulsion/calibrate_odrive', nucleo_message_ids.PropulsionCalibrateODrive)
 def propulsion_calibrate_odrive(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/odrive/clear_errors', 152)
+@nucleo_in('propulsion/odrive/clear_errors', nucleo_message_ids.PropulsionODriveClearErrors)
 def propulsion_odrive_clear_errors(msg):
     return _pb2.serialize(msg)
 
-@nucleo_out('propulsion/odrive/statistics', 180)
+@nucleo_out('propulsion/odrive/statistics', nucleo_message_ids.PropulsionODriveStatistics)
 def propulsion_odrive_statistics(payload):
     return _pb2.deserialize('goldo.nucleo.odrive.ClientStatistics', payload[:9])
 
 
-@nucleo_out('propulsion/odrive/axis_states', 181)
+@nucleo_out('propulsion/odrive/axis_states', nucleo_message_ids.PropulsionODriveAxisStates)
 def propulsion_odrive_axis_states(payload):
     msg = _pb2.deserialize('goldo.nucleo.odrive.AxisStates', payload)
     return msg
 
-@nucleo_out('propulsion/odrive/errors', 182)
+@nucleo_out('propulsion/odrive/errors', nucleo_message_ids.PropulsionODriveAxisErrors)
 def propulsion_odrive_errors(payload):
     msg = _pb2.deserialize('goldo.nucleo.odrive.AxisErrorStates', payload)
     return msg
 
-@nucleo_in('propulsion/clear_command_queue', 109)
+@nucleo_in('propulsion/clear_command_queue', nucleo_message_ids.PropulsionClearCommandQueue)
 def propulsion_clear_command_queue(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/cmd/translation', 140)
+@nucleo_in('propulsion/cmd/translation', nucleo_message_ids.PropulsionExecuteTranslation)
 def propulsion_execute_translation(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/cmd/reposition', 149)
+@nucleo_in('propulsion/cmd/reposition', nucleo_message_ids.PropulsionExecuteReposition)
 def propulsion_execute_reposition(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/cmd/measure_normal', 146)
+@nucleo_in('propulsion/cmd/measure_normal', nucleo_message_ids.PropulsionMeasureNormal)
 def propulsion_execute_reposition(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/cmd/move_to', 141)
+@nucleo_in('propulsion/cmd/move_to', nucleo_message_ids.PropulsionExecuteMoveTo)
 def propulsion_execute_move_to(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/cmd/rotation', 142)
+@nucleo_in('propulsion/cmd/rotation', nucleo_message_ids.PropulsionExecuteRotation)
 def propulsion_execute_rotation(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/cmd/point_to', 143)
+@nucleo_in('propulsion/cmd/point_to', nucleo_message_ids.PropulsionExecutePointTo)
 def propulsion_execute_point_to(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/cmd/point_to_back', 153)
+@nucleo_in('propulsion/cmd/point_to_back', nucleo_message_ids.PropulsionExecutePointToBack)
 def propulsion_execute_point_to(msg):
     return _pb2.serialize(msg)
 
-@nucleo_in('propulsion/cmd/face_direction', 144)
+@nucleo_in('propulsion/cmd/face_direction', nucleo_message_ids.PropulsionExecuteFaceDirection)
 def propulsion_execute_face_direction(msg):
     return _pb2.serialize(msg)
 
 
-@nucleo_out('propulsion/cmd_event', 130)
+@nucleo_out('propulsion/cmd_event', nucleo_message_ids.PropulsionCommandEvent)
 def propulsion_cmd_ack(payload):
     return _pb2.deserialize('goldo.nucleo.propulsion.CommandStatus', payload)
 
-@nucleo_out('propulsion/telemetry', 120)
+@nucleo_out('propulsion/telemetry', nucleo_message_ids.PropulsionTelemetry)
 def propulsion_telemetry(payload):
     msg = _msg_propulsion_Telemetry()
     vals = _unpack_propulsion_Telemetry(payload)
@@ -312,7 +321,7 @@ def propulsion_telemetry(payload):
     msg.error = vals[12]
     return msg
 
-@nucleo_out('propulsion/telemetry_ex', 121)
+@nucleo_out('propulsion/telemetry_ex', nucleo_message_ids.PropulsionTelemetryEx)
 def propulsion_telemetry_ex(payload):
     msg = _msg_propulsion_TelemetryEx()
     vals = _unpack_propulsion_TelemetryEx(payload)
@@ -330,41 +339,41 @@ def propulsion_telemetry_ex(payload):
     msg.error_yaw_rate = vals[11]
     return msg
 
-@nucleo_out('propulsion/state', 123)
+@nucleo_out('propulsion/state', nucleo_message_ids.PropulsionState)
 def propulsion_state(payload):
     return _pb2.deserialize('goldo.nucleo.propulsion.StateChange', payload)
 
-@nucleo_out('propulsion/odrive/telemetry', 124)
+@nucleo_out('propulsion/odrive/telemetry', nucleo_message_ids.PropulsionODriveTelemetry)
 def propulsion_telemetry(payload):
     return _pb2.deserialize('goldo.nucleo.odrive.Telemetry', payload)
 
-@nucleo_out('propulsion/odometry_stream', 126)
+@nucleo_out('propulsion/odometry_stream', nucleo_message_ids.PropulsionOdometryStream)
 def odometry_stream(payload):
     return _msg_BytesValue(value=payload)
 
-@nucleo_out('propulsion/odrive_stream', 127)
+@nucleo_out('propulsion/odrive_stream', nucleo_message_ids.PropulsionODriveStream)
 def odrive_stream(payload):
     return _msg_BytesValue(value=payload)
 
-@nucleo_in('propulsion/scope/config/set', 111)
+@nucleo_in('propulsion/scope/config/set', nucleo_message_ids.PropulsionScopeConfig)
 def propulsion_scope_config_set(msg):
     if len(msg.channels) > 8:
         raise RuntimeError('ScopeConfig channels count > 8')
     return struct.pack('<HH', msg.period, len(msg.channels)) + b''.join([_pb2.serialize(channel) for channel in msg.channels])
 
-@nucleo_out('propulsion/scope/data', 125)
+@nucleo_out('propulsion/scope/data', nucleo_message_ids.PropulsionScopeData)
 def propulsion_scope(payload):
     msg = _sym_db.GetSymbol('goldo.nucleo.ScopeData')()
     msg.timestamp = struct.unpack('<H', payload[0:2])[0]
     msg.data = payload[2:]
     return msg
 
-@nucleo_in('dbg_goldo', 29)
+@nucleo_in('dbg_goldo', nucleo_message_ids.DbgGoldo)
 def dbg_goldo_in(msg):
     #print ("in : {:x}".format(msg.value))
     return struct.pack('<I', msg.value)
 
-@nucleo_out('dbg_goldo', 29)
+@nucleo_out('dbg_goldo', nucleo_message_ids.DbgGoldo)
 def dbg_goldo_out(payload):
     val = struct.unpack("<I", payload)
     #print ("out : {:x}".format(val[0]))
